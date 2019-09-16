e13089f1000e885014b69481016cb4d7d36f692a
Aditya Milind Mate <53221701+AdityaM8@users.noreply.github.com>
2019-09-16 19:59:37 +0530

    Add files via upload

---
+++ fo.php
@@ -0,0 +1,3 @@
+<?php
+echo "WELCOME; ".$_POST['fname']."<br>".$_POST[ 'email' ]."<br>".$_POST[ 'date' ]."<br>".$_POST[ 'adrs' ]."<br>".$_POST[ 'phn' ]."<br>".$_POST[ 'gender' ]."<br>".$_POST[ 'gt' ];
+?>
